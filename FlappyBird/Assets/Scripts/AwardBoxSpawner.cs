5f0fee7eda9a569db1a514310ba4fe29c3944ce2
Mehmet Yıldırım <mehmetyildirim.developer@gmail.com>
2022-02-09 10:21:04 +0300

    + refactor Spawner

--- FlappyBird/Assets/Scripts/AwardBoxSpawner.cs
+++ FlappyBird/Assets/Scripts/AwardBoxSpawner.cs
@@ -17,7 +17,7 @@ public class AwardBoxSpawner : MonoBehaviour
         {
 
             Instantiate(AwardBox, new Vector3(1.5f, Random.Range(-0.4f, 0.4f), 1f), Quaternion.identity);//spawnlýyor
-            yield return new WaitForSeconds(Random.Range(5f, 15f));//Bir saniye bekle kodunu yazdik ///bekliyor
+            yield return new WaitForSeconds(Random.Range(10f, 20f));//Bir saniye bekle kodunu yazdik ///bekliyor
 
         }
 
